152161ecfce70be05c55daa411218ed9daddb71c
Athira P Mohandas <athirapmohandas@gmail.com>
2021-10-21 09:38:13 +0530

    msg

---
+++ javascript/DOM/bank_application/script.js
@@ -0,0 +1,13 @@
+
+
+class Bank{
+    createAccount(){
+         let first_name=fname.value;
+         //let acno=accno.value;
+         //let password=pwd.value;
+        //  result.innerHTML=first_name
+        console.log(first_name);
+        //alert("hello")
+    }
+}
+var bank=new Bank();
---
+++ javascript/DOM/script1.js
@@ -0,0 +1,9 @@
+var person={
+    name:"ram",
+    getName: function(){
+        return this.name;
+    }
+}
+console.log(person.getName());
+//global ==server side
+//window== client side
